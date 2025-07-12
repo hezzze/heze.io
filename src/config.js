3773f857e114dd17c8542b74420da301fed95678
heze <zzcreation2022@gmail.com>
2025-07-12 11:36:13 +0800

    add config.js

---
+++ src/config.js
@@ -0,0 +1,14 @@
+// Configuration file for the application
+// This file contains settings that can be customized per environment
+
+const config = {
+  video: {
+    // Default video URL - you can replace this with your own video
+    url: 'https://meng2x.oss-cn-beijing.aliyuncs.com/test_m2_api/output_480p_0b778bbbad.webm',
+    // Default poster image - you can replace this with your own image
+    poster: 'https://meng2x.oss-cn-beijing.aliyuncs.com/test_m2_api/output_480p_sm_1a2638975f.png'
+  },
+  apiUrl: 'https://app.meng2x.com'
+};
+
+export default config;
